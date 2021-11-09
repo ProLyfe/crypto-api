4e2a623ee04e50c750d1ee09903df73bd8577560
Rayane Emamdee <rayane@MacBook-Pro-de-Rayane.local>
2021-11-09 15:51:52 +0100

    [test] add console test

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const fastify = require('fastify')({ logger: true });
 const fetch = require('node-fetch');
 const PORT = process.env.PORT || 3000;
 
-
 fastify.get('/crypto', async(_, res) => {
     try {
         const response = await fetch('https://api.binance.com/api/v3/ticker/price');
         const body = await response.text();
+        console.log("Je suis un test")
         res.send(body);
     } catch (err) {
         console.error(err);
     };
 });
 
-console.log('Salut tout le monde');
+
 
 fastify.listen(PORT);
